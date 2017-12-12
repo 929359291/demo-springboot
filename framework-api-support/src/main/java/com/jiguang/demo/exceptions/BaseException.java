a8697b2a83c539e24bd47d599fc4fcb077f4e16e
liups <liups@jiguang.cn>
2017-12-12 15:26:35 +0800

    修改eureka地址

---
+++ framework-api-support/src/main/java/com/jiguang/demo/exceptions/BaseException.java
@@ -0,0 +1,25 @@
+package com.jiguang.demo.exceptions;
+
+/**
+ * @author liups
+ * @create 2017/12/12
+ */
+public class BaseException extends RuntimeException {
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+
+    protected BaseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
