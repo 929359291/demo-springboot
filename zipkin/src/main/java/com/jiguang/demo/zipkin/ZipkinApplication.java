7f0a2333250e32a4c6e1e32d1088fdea8829937a
liups <liups@jiguang.cn>
2017-12-13 19:56:05 +0800

    zipkin服务部署

---
+++ zipkin/src/main/java/com/jiguang/demo/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package com.jiguang.demo.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * @author liups
+ * @create 2017/12/13
+ */
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
